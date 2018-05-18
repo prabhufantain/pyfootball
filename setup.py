e56f4450cd10d5ed2e076e3a02b5860badbd7126
prabhupy07 <prabuaut07@gmail.com>
2018-05-18 14:55:39 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name = 'py-football',
+    version = '1.0.3',
+    description = 'Football Wrapper',
+    long_description = 'Football Wrapper',
+
+    author = 'Prabhu Rajendran',
+    author_email = 'prabhu@fantain.com',
+    url = 'https://github.com/prabhufantain/pyfootball',
+    package_dir={'': 'src'},
+    packages=[''],
+    install_requires=['certifi==2017.4.17', 'chardet==3.0.4', 'idna==2.5',
+                      'requests==2.18.1', 'urllib3==1.21.1'],
+    entry_points='''
+        [console_scripts]
+        pyfootball=src.pyfootball:RcaApp
+    '''
+)
